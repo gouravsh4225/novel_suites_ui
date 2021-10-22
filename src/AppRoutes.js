86b16ee8039f0bb272860c8149ba7bcdb9060c8e
gouravsh4225 <gouravsh4225@gmail.com>
2021-10-22 13:33:44 +0530

    removed ts-lint error

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -8,6 +8,7 @@ import HeaderFooterLayout from "./PageLayout/HeaderFooterLayout/HeaderFooterLayo
 import ContactUs from "./Pages/ContactUs/ContactUs";
 
 const AppRoutes = () => {
+  console.log("deploying-->");
   return (
     <Router>
       <Switch>
