a1a73936a73609b2812a616b184480deeee6dc85
gouravsh4225 <gouravsh4225@gmail.com>
2021-11-10 21:05:42 +0530

    add new skeleton UI component and add on Location Component

--- src/Pages/Location/Location.js
+++ src/Pages/Location/Location.js
@@ -1,23 +1,23 @@
 import React, { Fragment, useEffect, useState } from "react";
-import WorkUnderProcess from "../../Components/WorkUnderProcess/WorkUnderProcess";
-import NovelSuitesButton from "../../SharedComponents/UI_Elements/NovelSuitesButton/NovelSuitesButton";
+import NovelSkeleton from "../../SharedComponents/NovelSkeleton/NovelSkeleton";
+import LocationCard from "./LocationCard";
 import { getAllLocation } from "../../Services/Location/LocationService";
 import "./Location.scss";
-import NovelLoader from "../../SharedComponents/NovelLoader/NovelLoader";
 
-const locationArray = () => {
-  return [
-    {
-      id: 1,
-      country_location: "Dwarka, Delhi",
-      location_address:
-        "Plot - 339, Block B, Sec - 19, Dwarka, New Delhi - 110075",
-      contact_number: "+918383019368",
-      contact_email: "novelsuites@gmail.com",
-      location_address_imageurl:
-        "https://res.cloudinary.com/arbor1221/image/upload/v1498121225/Consulting_Advisory_Professional_services_2_ikqokw.jpg",
-    },
-  ];
+const LocationSkeletonCards = () => {
+  return [1, 2, 3, 4].map((item) => (
+    <NovelSkeleton.Card key={item}>
+      <NovelSkeleton.CardHeader>
+        <NovelSkeleton.FullWidthImage />
+      </NovelSkeleton.CardHeader>
+      <NovelSkeleton.HalfHeading />
+      <NovelSkeleton.FullHeading />
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <NovelSkeleton.Button />
+        <NovelSkeleton.Button />
+      </div>
+    </NovelSkeleton.Card>
+  ));
 };
 
 const Location = () => {
@@ -27,7 +27,6 @@ const Location = () => {
   useEffect(() => {
     getAllLocation()
       .then((res) => {
-        console.log(res, "response in component");
         setLocationList(res);
         setIsLoading(false);
       })
@@ -35,15 +34,9 @@ const Location = () => {
         console.log(error);
       });
   }, []);
-  const browserLocationRoomHandler = (e, location) => {
-    console.log("e,-->", e, location);
-  };
-  const learnMoreLocationHandler = () => {
-    console.log("hey--");
-  };
+
   return (
     <Fragment>
-      <NovelLoader isOpen={isLoading} />
       <div className="location-wrapper container">
         <div className="location-wrapper-title text-center">
           <h1 className="location-wrapper-heading">Locations</h1>
@@ -52,64 +45,12 @@ const Location = () => {
           </p>
         </div>
         <article className="location-card grid-container grid-container-fill grid-gap-1">
-          {locationList.map((locationItem) => (
-            <div className="novel-card" key={locationItem.location_id}>
-              <div className="novel-card-img-wrapper">
-                <img
-                  src={locationItem.location_address_imageurl}
-                  className="location-image"
-                  loading="lazy"
-                  alt="location-address"
-                />
-              </div>
-              <div className="novel-card-content p-1 location-card-content">
-                <h2 className="location-title mb-1 mt-0">
-                  {locationItem.location_short_address}
-                </h2>
-                <p className="location-address mb-1">
-                  {locationItem.location_full_address}
-                </p>
-                <div
-                  tabIndex="0"
-                  className="mb-1 location-address text-decoration-none"
-                >
-                  <span>
-                    <i className="fa fa-phone" aria-hidden="true"></i>
-                  </span>
-                  <span className="ml-1">
-                    {locationItem?.contact_details.phone_number}
-                  </span>
-                </div>
-                <div
-                  tabIndex="0"
-                  className="mb-1 location-address text-decoration-none letter-spacing-3"
-                >
-                  <span>
-                    <i className="fa fa-envelope" aria-hidden="true"></i>
-                  </span>
-                  <span className="ml-1">
-                    {locationItem?.contact_details.email_address}
-                  </span>
-                </div>
-                <div className="grid-container grid-gap-1 location-buttons">
-                  <NovelSuitesButton
-                    type="button"
-                    className="novel-button--primary "
-                    buttonLabel="Book A room"
-                    onClick={() => browserLocationRoomHandler(locationItem)}
-                  />
-                  <NovelSuitesButton
-                    type="button"
-                    className="novel-button--secondary-text "
-                    buttonLabel="Learn More"
-                    onClick={() => learnMoreLocationHandler(locationItem)}
-                  />
-                </div>
-              </div>
-            </div>
-          ))}
+          {isLoading ? (
+            LocationSkeletonCards()
+          ) : (
+            <LocationCard locationList={locationList} />
+          )}
         </article>
-        <WorkUnderProcess />
       </div>
     </Fragment>
   );
---
+++ src/SharedComponents/NovelSkeleton/NovelSkeleton.js
@@ -0,0 +1,116 @@
+import React from "react";
+import UIElementHelper from "../UI_Elements/UI_Element_helpers";
+import "./NovelSkeleton.scss";
+
+const addAllSkeletonCssClass = (initalClass, propsClasess) => {
+  return UIElementHelper.getllClasses(initalClass, propsClasess);
+};
+
+const NovelSkeleton = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+const RoundedImage = ({ children, className }) => {
+  return (
+    <div
+      className={addAllSkeletonCssClass("skeleton-rounded-image", className)}
+    ></div>
+  );
+};
+
+const SquareImage = ({ children, className }) => {
+  return (
+    <div
+      className={addAllSkeletonCssClass("skeleton-sqaure-image", className)}
+    ></div>
+  );
+};
+
+const FullWidthImage = ({ children, className }) => {
+  return (
+    <div
+      className={addAllSkeletonCssClass("skeleton-full-width-image", className)}
+    ></div>
+  );
+};
+
+const HalfHeading = ({ children, className }) => {
+  return (
+    <div
+      className={addAllSkeletonCssClass("skeleton-half-heading", className)}
+    ></div>
+  );
+};
+const FullHeading = ({ children, className }) => {
+  return (
+    <div
+      className={addAllSkeletonCssClass("skeleton-full-heading", className)}
+    ></div>
+  );
+};
+
+/**
+ * skeleton Card Start
+ */
+const Card = ({ children, className }) => {
+  return (
+    <div className={addAllSkeletonCssClass("skeleton-card", className)}>
+      {children}
+    </div>
+  );
+};
+
+const CardHeader = ({ children, className }) => {
+  return (
+    <div className={addAllSkeletonCssClass("skeleton-card-header", className)}>
+      {children}
+    </div>
+  );
+};
+
+const CardFooter = ({ children, className }) => {
+  return (
+    <div className={addAllSkeletonCssClass("skeleton-card-footer", className)}>
+      {children}
+    </div>
+  );
+};
+
+const CardContent = ({ children, className }) => {
+  return (
+    <div className={addAllSkeletonCssClass("skeleton-card-content", className)}>
+      {children}
+    </div>
+  );
+};
+
+/**
+ * Novel Skeleton Button
+ * start
+ */
+
+const Button = ({ children, className }) => {
+  return (
+    <div className={addAllSkeletonCssClass("skeleton-button", className)}>
+      {children}
+    </div>
+  );
+};
+
+/**
+ * Novel Skeleton Button
+ * end
+ */
+
+NovelSkeleton.RoundedImage = RoundedImage;
+NovelSkeleton.SquareImage = SquareImage;
+NovelSkeleton.FullWidthImage = FullWidthImage;
+NovelSkeleton.HalfHeading = HalfHeading;
+NovelSkeleton.FullHeading = FullHeading;
+NovelSkeleton.Card = Card;
+NovelSkeleton.CardHeader = CardHeader;
+NovelSkeleton.CardFooter = CardFooter;
+NovelSkeleton.CardContent = CardContent;
+NovelSkeleton.Button = Button;
+
+export default NovelSkeleton;
